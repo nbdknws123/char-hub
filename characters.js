53da0f13af1bbb460f2ed53f8ad9f255fceb2e62
nbdknws123 <gia70021221@gmail.com>
2026-08-13 14:44:38 +0700

    characters.js

---
+++ characters.js
@@ -0,0 +1,8 @@
+const characters = [
+    {
+    name: "Trần Gia Huy",
+    image: "https://i.pinimg.com/1200x/f5/1a/fe/f51afe9ed0550ea5a2643b5696d98254.jpg",
+    description: "bạn lớp trưởng ngại ngùng, hiền queo, crush mình 3 năm. siu đáng yêu",
+    tags: ["BL", "Cún con", "Chữa lành", "TXVT", "Học bá"],
+    link: "https://aistudio.google.com/prompts/1VWPjBnBQ_J03uaJkHIjqEGDtCb6pco4s"
+},

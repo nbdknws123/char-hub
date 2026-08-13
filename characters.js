3d43fd154ca84650fcd666519bde1f87f5126783
nbdknws123 <gia70021221@gmail.com>
2026-08-13 15:15:11 +0700

    characters.js

--- characters.js
+++ characters.js
@@ -1,23 +1,23 @@
 const characters = [
 
     {
-        id: "co-hai",
+        id: "tran-gia-huy",
 
-        name: "Cố Hải",
+        name: "TRẦN GIA HUY",
 
-        image: "LINK_ANH_CUA_CO_HAI",
+        image: "https://i.pinimg.com/1200x/f5/1a/fe/f51afe9ed0550ea5a2643b5696d98254.jpg",
 
         description:
-            "Một thiếu gia kiêu ngạo, quyết đoán và mang khí chất áp đảo. Bề ngoài lạnh lùng nhưng bên trong cực kỳ cố chấp.",
+            "bạn lớp trưởng ngại ngùng, hiền queo, crush mình 3 năm. siu đáng yêu",
 
         tags: [
             "BL",
-            "CEO",
-            "Rich",
-            "Possessive"
+            "txvt",
+            "Cún con",
+            "Healing"
         ],
 
-        creator: "Your Name",
+        creator: "Risie yêu chồng",
 
         featured: true,
 
@@ -25,15 +25,11 @@ const characters = [
 
         createdAt: "2026-08-13",
 
-        platforms: [
+        platforms: [ 
             {
-                name: "Platform 1",
-                url: "https://example.com"
+                name: "GGAI",
+                url: "https://aistudio.google.com/prompts/1VWPjBnBQ_J03uaJkHIjqEGDtCb6pco4s"
             },
-            {
-                name: "Platform 2",
-                url: "https://example.com"
-            }
         ]
     },
 
